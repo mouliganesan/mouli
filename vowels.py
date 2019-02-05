11781a898dd3cd673b8b5d459da65551e560d34e
mouliganesan <47343992+mouliganesan@users.noreply.github.com>
2019-02-05 11:12:00 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,8 @@
+print("enter '0' for exit.");
+ch = input("enter any character:");
+if ch=='0';
+else:
+if(ch=='a' or ch=='e'ch=='i'ch=='o'ch=='u'ch=='A'ch=='E'ch=='I'ch=='O'ch=='U');
+print(ch,"is a vowel");
+else
+print(ch,"is not a vowel");
